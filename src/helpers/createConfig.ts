9b3aadcc505f7834c1ee343e091be0d7dbd04cb9
Daniil Parfenov <dan.exxx1st@gmail.com>
2021-02-01 14:11:43 +0300

    Change env from APP_PORT to PORT

--- src/helpers/createConfig.ts
+++ src/helpers/createConfig.ts
@@ -1,6 +1,6 @@
 export function createConfig() {
   const config = {
-    APP_PORT: process.env.APP_PORT || "8080",
+    APP_PORT: process.env.PORT || "8080",
     DB_NAME: process.env.DB_NAME || "vudo",
     DB_HOST: process.env.DB_HOST || "localhost",
     DB_PORT: process.env.DB_PORT || 5432,
